de89fb0cd68bdf69926c70a2957347eac57517f2
Yixiu <yc3691@nyu.edu>
2020-08-03 13:15:57 -0400

    Added specialty and updated vet class.

---
+++ pet-clinic-data/src/main/java/com/kyotoyx/petclinic/model/Specialty.java
@@ -0,0 +1,14 @@
+package com.kyotoyx.petclinic.model;
+
+public class Specialty {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
