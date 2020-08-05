7d95fd4e2a8e7e14eee75d55ec62619730de4cac
Yixiu <yc3691@nyu.edu>
2020-08-05 11:47:06 -0400

    Added all JPA annotations.

--- pet-clinic-data/src/main/java/com/kyotoyx/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/kyotoyx/petclinic/model/Vet.java
@@ -1,10 +1,21 @@
 package com.kyotoyx.petclinic.model;
 
+import org.springframework.data.repository.cdi.Eager;
+
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "vet_specialties",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speicialty_id")
+    )
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
